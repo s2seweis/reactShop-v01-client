8e325697417ea91f72d79a740660499ca7ee59d0
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-11-27 21:17:22 +0100

    delete the old dist folder and build it new

---
+++ dist/js/449.86bfdd61168fa96af5e8.js
@@ -0,0 +1,29 @@
+"use strict";
+(self["webpackChunkecommerce_react"] = self["webpackChunkecommerce_react"] || []).push([[449],{
+
+/***/ 58449:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(67294);
+
+// Just add this feature if you want :P
+var UserWishListTab = function UserWishListTab() {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "loader",
+    style: {
+      minHeight: '80vh'
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h3", null, "More Settings"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("strong", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
+    className: "text-subtle"
+  }, "You don't have more settings currently")));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (UserWishListTab);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=449.86bfdd61168fa96af5e8.js.map
